734e1c1b56cacec280987e673268b4cb032c433f
jsango2 <jure.sango@gmail.com>
2023-03-03 17:24:09 +0100

    new commit

---
+++ components/rimac/FullHd150.js
@@ -0,0 +1,51 @@
+import { render } from "react-dom";
+import { useEffect, useRef } from "react";
+import { VideoScroll } from "react-video-scroll";
+import usePageBottom from "../helper/pageBottom";
+
+const setStyles = (wrapperEl, videoEl, playbackRate) => {
+  wrapperEl.style.marginTop = `calc(180% - ${
+    Math.floor(videoEl.duration) * playbackRate + "px"
+  })`;
+  wrapperEl.style.marginBottom = `calc(180% - ${
+    Math.floor(videoEl.duration) * playbackRate + "px"
+  })`;
+};
+
+const RC4 = () => {
+  const reachedBottom = usePageBottom();
+  console.log("reachedBottom", reachedBottom);
+  useEffect(() => {
+    reachedBottom && window.scrollTo(0, 0);
+  }, [reachedBottom]);
+
+  return (
+    <div style={{ height: "1380vh", position: "relative" }}>
+      <VideoScroll
+        onLoad={(props) =>
+          setStyles(props.wrapperEl, props.videoEl, props.playbackRate)
+        }
+        playbackRate={45}
+        style={{ position: "sticky" }}
+      >
+        <video
+          tabIndex="0"
+          autobuffer="autobuffer"
+          preload="auto"
+          //   style={{ width: "100%", objectFit: "contain" }}
+          style={{
+            position: "fixed",
+            width: "100vw",
+            touchAction: "none",
+            left: "0",
+            top: "0",
+          }}
+          playsInline
+        >
+          <source type="video/mp4" src="./RimacWeb.mp4" />
+        </video>
+      </VideoScroll>
+    </div>
+  );
+};
+export default RC4;
