5ab40fbbb4d01490807e9b8ea3be96e4f00ce707
jsango2 <jure.sango@gmail.com>
2023-03-03 19:38:22 +0100

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState, useRef } from "react";
 import dynamic from "next/dynamic";
+import usewindowsize from "../components/helper/usewindowsize";
 
 const Rimac = dynamic(() => import("../components/rimac/FullHd150"), {
   ssr: false,
 });
+const RimacMobile = dynamic(() => import("../components/rimac/FullHdMobile2"), {
+  ssr: false,
+});
 
 export default function Index() {
-  return (
-    <div>
-      <Rimac />
-    </div> 
-  );
+  const size = usewindowsize();
+  return <div>{size.width > 600 ? <Rimac /> : <RimacMobile />}</div>;
 }
